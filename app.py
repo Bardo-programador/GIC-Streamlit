c91aabe472e776565091fb75b60a3b90a697368d
samuel <developer.snowstorm590@passinbox.com>
2025-07-15 16:41:14 -0300

    Atualizações do app

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly.express as px
 import api
 
-st.title("Insights sobre Licitações e Contratos - Cidades Inteligentes")
+
 st.markdown("""
-Este app ajuda a explorar dados do PNCP focados em projetos de **cidades inteligentes**.
+Este app ajuda a explorar dados do [PNCP](https://www.gov.br/pncp/pt-br) focados em projetos de **cidades inteligentes**.
 """)
 
 # Carregar dados
@@ -25,20 +25,24 @@ def load_data():
 df = load_data()
 
 # ---- FILTROS NA SIDEBAR
-st.sidebar.header("🎛️ Filtros")
+with st.sidebar:
+    st.image("img/logo.jpeg", width=200)
+    st.header("🎛️ Filtros")
 
-orgaos = st.sidebar.multiselect("Órgãos", sorted(df['orgao_nome'].dropna().unique()))
-cidades = st.sidebar.multiselect("Municípios", sorted(df['municipio_nome'].dropna().unique()))
-ufs = st.sidebar.multiselect("UF", sorted(df['uf'].dropna().unique()))
-modalidades = st.sidebar.multiselect("Modalidade", sorted(df['modalidade_licitacao_nome'].dropna().unique()))
-anos = st.sidebar.multiselect("Ano", sorted(df['ano'].dropna().unique()))
-valor_min, valor_max = st.sidebar.slider("Valor Global (R$)", 
-                                         float(df['valor_global'].min()), 
-                                         float(df['valor_global'].max()), 
-                                         (float(df['valor_global'].min()), float(df['valor_global'].max())))
+    orgaos = st.multiselect("Órgãos", sorted(df['orgao_nome'].dropna().unique()))
+    cidades = st.multiselect("Municípios", sorted(df['municipio_nome'].dropna().unique()))
+    ufs = st.multiselect("UF", sorted(df['uf'].dropna().unique()))
+    modalidades = st.multiselect("Modalidade", sorted(df['modalidade_licitacao_nome'].dropna().unique()))
+    anos = st.multiselect("Ano", sorted(df['ano'].dropna().unique()))
+    valor_min, valor_max = st.slider("Valor Global (R$)", 
+                                            float(df['valor_global'].min()), 
+                                            float(df['valor_global'].max()), 
+                                            (float(df['valor_global'].min()), float(df['valor_global'].max())))
 
 # ---- FILTRAGEM
 filtered_df = df.copy()
+filtered_df["valor_global_por_milhao"] = filtered_df["valor_global"] / 1000000
+
 if orgaos:
     filtered_df = filtered_df[filtered_df['orgao_nome'].isin(orgaos)]
 if cidades:
@@ -49,29 +53,42 @@ if modalidades:
     filtered_df = filtered_df[filtered_df['modalidade_licitacao_nome'].isin(modalidades)]
 if anos:
     filtered_df = filtered_df[filtered_df['ano'].isin(anos)]
-
 filtered_df = filtered_df[(filtered_df['valor_global'] >= valor_min) & (filtered_df['valor_global'] <= valor_max)]
-
 # ---- MÉTRICAS
 st.subheader("📈 Visão Geral")
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 col1.metric("Número de registros", len(filtered_df))
-col2.metric("Valor total (R$)", f"{filtered_df['valor_global'].sum():,.2f}")
+col2.metric("Valor total (Milhão)", f"R${filtered_df['valor_global_por_milhao'].sum():,.3f}")
 col3.metric("Órgãos distintos", filtered_df['orgao_nome'].nunique())
+col4.metric("Municípios distintos", filtered_df['municipio_nome'].nunique())
 
 # ---- GRÁFICOS
 if not filtered_df.empty:
-    st.subheader("💰 Valor global por órgão")
-    fig1 = px.bar(filtered_df.groupby('orgao_nome')['valor_global'].sum().reset_index().sort_values('valor_global', ascending=False),
-                  x='orgao_nome', y='valor_global',
-                  labels={'orgao_nome':'Órgão', 'valor_global':'Valor Total (R$)'},
+    st.subheader("💰 Top 10 orgãos com maior gasto")
+    fig1 = px.bar(filtered_df.groupby('orgao_nome')['valor_global_por_milhao'].sum().reset_index().sort_values('valor_global_por_milhao', ascending=False).head(10),
+                  x='orgao_nome', y='valor_global_por_milhao',
+                  labels={'orgao_nome':'Órgão', 'valor_global_por_milhao':'Valor Total (em milhão)'},
                   text_auto=True)
     fig1.update_layout(xaxis_tickangle=-45)
+    fig1.update_traces(
+    texttemplate='%{y:,.3f}',  # formata com milhar
+    textposition='outside',      # ou 'auto' / 'inside'
+    hovertemplate='%{label}<br>Valor Global: R$%{value:,.3f} milhão ' # formata texto da barra
+    )
+
+    fig1.update_layout(
+    yaxis_tickformat=',',       # também remove o K do eixo Y
+    uniformtext_minsize=8,
+    uniformtext_mode='hide'
+    )
     st.plotly_chart(fig1, use_container_width=True)
 
     st.subheader("🗺 Distribuição por município")
     fig2 = px.pie(filtered_df, names='municipio_nome', values='valor_global',
-                  title="Participação no Valor Global por Município")
+                  labels={'municipio_nome':'Município', 'valor_global':'Valor Total (R$)'},  
+                  title="Participação no Valor Global por Município", subtitle="")
+    fig2.update_traces(hovertemplate='%{label}<br>Participação: %{percent}',
+                        textinfo='none')  # aqui você controla o texto
     st.plotly_chart(fig2, use_container_width=True)
 
 # ---- TABELA DETALHADA
@@ -82,7 +99,7 @@ st.dataframe(filtered_df[['data_assinatura','orgao_nome','municipio_nome','uf',
 # ---- DOWNLOAD
 st.download_button("📥 Baixar dados filtrados (CSV)",
                    data=filtered_df.to_csv(index=False).encode('utf-8'),
-                   file_name="dados_filtrados.csv",
+                   file_name="data.csv",
                    mime='text/csv')
 
 # ---- GLOSSÁRIO
